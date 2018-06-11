775928dcab188bc21f5381a3520c06fe581cc42b
ady-chow <zhouxinyuan@p1.com>
2018-06-11 18:41:25 +0800

    abstract style; add new anims;

--- app/src/main/java/com/ady/anim_engine_learn/AnimHelper.java
+++ app/src/main/java/com/ady/anim_engine_learn/AnimHelper.java
@@ -3,9 +3,7 @@ package com.ady.anim_engine_learn;
 import android.support.v4.util.Pair;
 import java.util.HashMap;
 
-/**
- * Created by ady on 2018/2/28.
- */
+/** Created by ady on 2018/2/28. */
 public class AnimHelper {
 
   public static HashMap<String, Pair<String, String>> ANIMATIONS = new HashMap<>();
@@ -17,18 +15,22 @@ public class AnimHelper {
   public static final String WELCOME_OMI_BG = "welcome_omi_bg";
   public static final String WELCOME_OMI_SOUND = "animations/welcome_omi/sound.mp3";
   public static final String ICE_BREAK_LAUGH = "ice_break_laugh";
+  public static final String ICE_BREAK_PEACH = "ice_break_peach";
+  public static final String ICE_BREAK_KISS = "ice_break_kiss";
 
   static {
-    ANIMATIONS.put(LIKE, Cu.pair("animations/like/config.xml", "animations/like/pic"));
-    ANIMATIONS.put(PASS, Cu.pair("animations/pass/config.xml", "animations/pass/pic"));
-    ANIMATIONS.put(MATCH, Cu.pair("animations/match/config.xml", "animations/match/pic"));
-    ANIMATIONS.put(MATCH_BG, Cu.pair("animations/match_bg/config.xml", "animations/match_bg/pic"));
-    ANIMATIONS.put(
-        WELCOME_OMI, Cu.pair("animations/welcome_omi/config.xml", "animations/welcome_omi/pic"));
-    ANIMATIONS.put(
-        WELCOME_OMI_BG,
-        Cu.pair("animations/welcome_omi_bg/config.xml", "animations/welcome_omi_bg/pic"));
-    ANIMATIONS.put(ICE_BREAK_LAUGH,
-        Cu.pair("animations/ice_break_laugh/config.xml", "animations/ice_break_laugh/pic"));
+    ANIMATIONS.put(LIKE, ANIM_PAIR(LIKE));
+    ANIMATIONS.put(PASS, ANIM_PAIR(PASS));
+    ANIMATIONS.put(MATCH, ANIM_PAIR(MATCH));
+    ANIMATIONS.put(MATCH_BG, ANIM_PAIR(MATCH_BG));
+    ANIMATIONS.put(WELCOME_OMI, ANIM_PAIR(WELCOME_OMI));
+    ANIMATIONS.put(WELCOME_OMI_BG, ANIM_PAIR(WELCOME_OMI_BG));
+    ANIMATIONS.put(ICE_BREAK_LAUGH, ANIM_PAIR(ICE_BREAK_LAUGH));
+    ANIMATIONS.put(ICE_BREAK_PEACH, ANIM_PAIR(ICE_BREAK_PEACH));
+    ANIMATIONS.put(ICE_BREAK_KISS, ANIM_PAIR(ICE_BREAK_KISS));
+  }
+
+  private static Pair<String, String> ANIM_PAIR(String name) {
+    return Cu.pair("animations/" + name + "/config.xml", "animations/" + name + "/pic");
   }
 }
--- app/src/main/java/com/ady/anim_engine_learn/MainAct.java
+++ app/src/main/java/com/ady/anim_engine_learn/MainAct.java
@@ -1,7 +1,9 @@
 package com.ady.anim_engine_learn;
 
 import static com.ady.anim_engine_learn.AnimHelper.ANIMATIONS;
+import static com.ady.anim_engine_learn.AnimHelper.ICE_BREAK_KISS;
 import static com.ady.anim_engine_learn.AnimHelper.ICE_BREAK_LAUGH;
+import static com.ady.anim_engine_learn.AnimHelper.ICE_BREAK_PEACH;
 import static com.ady.anim_engine_learn.AnimHelper.LIKE;
 
 import android.app.Activity;
@@ -12,19 +14,20 @@ import android.view.View;
 import android.widget.Button;
 import com.sunshine.engine.bone.StageView;
 
-/**
- * Created by ady on 2018/6/6.
- */
-
+/** Created by ady on 2018/6/6. */
 public class MainAct extends Activity {
+
+  private boolean showingLayout;
 
   private static final String BONE_ANIM_LIKE = "bone_anim_like";
   private static final String BONE_ANIM_LAUGH = "bone_anim_laugh";
+  private static final String BONE_ANIM_PEACH = "bone_anim_peach";
+  private static final String BONE_ANIM_KISS = "bone_anim_kiss";
 
   Button _btn_like;
   Button _btn_laugh;
-
-  private boolean showingLayout;
+  Button _btn_peach;
+  Button _btn_kiss;
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,6 +35,8 @@ public class MainAct extends Activity {
     setContentView(R.layout.act_main);
     _btn_like = findViewById(R.id.bone_anim_like);
     _btn_laugh = findViewById(R.id.bone_anim_laugh);
+    _btn_peach = findViewById(R.id.bone_anim_peach);
+    _btn_kiss = findViewById(R.id.bone_anim_kiss);
   }
 
   public void show(View v) {
@@ -45,6 +50,12 @@ public class MainAct extends Activity {
         break;
       case BONE_ANIM_LAUGH:
         animLaugh();
+        break;
+      case BONE_ANIM_PEACH:
+        animPeach();
+        break;
+      case BONE_ANIM_KISS:
+        animKiss();
         break;
     }
   }
@@ -65,6 +76,23 @@ public class MainAct extends Activity {
     laugh.autoStop(false);
     laugh.isRepeat(true);
     laugh.setPercent(1f, 4000);
+  }
+
+  private void animPeach() {
+    StageView peach = findViewById(R.id.peach);
+    Pair<String, String> pair = ANIMATIONS.get(ICE_BREAK_PEACH);
+    peach.play(pair.first, pair.second);
+    peach.autoStop(false);
+    peach.setPercent(1f, 3500);
+  }
+
+  private void animKiss() {
+    StageView kiss = findViewById(R.id.kiss);
+    Pair<String, String> pair = ANIMATIONS.get(ICE_BREAK_KISS);
+    kiss.play(pair.first, pair.second);
+    kiss.autoStop(false);
+    kiss.isRepeat(true);
+    kiss.setPercent(1f, 5000);
   }
 
   @Override
